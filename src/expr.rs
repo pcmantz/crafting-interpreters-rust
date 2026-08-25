ff7a11ecef6263bfa3593a67ff198f6b16c4ed9a
Paul C. Mantz <pcmantz@mcpantz.org>
2026-08-25 17:06:41 -0500

    fix display sexpr output

--- src/expr.rs
+++ src/expr.rs
@@ -68,8 +68,8 @@ impl fmt::Display for Expr {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Expr::Literal(e) => write!(f, "{}", e.value),
-            Expr::Unary(e) => write!(f, "({} {})", e.operator, e.right),
-            Expr::Binary(e) => write!(f, "({} {} {})", e.operator, e.left, e.right),
+            Expr::Unary(e) => write!(f, "({} {})", e.operator.lexeme, e.right),
+            Expr::Binary(e) => write!(f, "({} {} {})", e.operator.lexeme, e.left, e.right),
             Expr::Grouping(e) => write!(f, "(group {})", e.expression),
         }
     }
