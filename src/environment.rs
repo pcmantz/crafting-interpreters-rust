2346900cbb629c2cb5a1764837d433d91932500e
Paul C. Mantz <pcmantz@mcpantz.org>
2026-09-04 19:49:03 -0500

    add enclosing environment

--- src/environment.rs
+++ src/environment.rs
@@ -8,21 +8,35 @@ use crate::error::*;
 use crate::token::*;
 use crate::value::*;
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct Environment {
     values: HashMap<String, Value>,
+    enclosing: Option<Box<Environment>>,
 }
 
 impl Environment {
+    pub fn with_enclosing(enclosing: Environment) -> Self {
+        Self {
+            enclosing: Some(Box::new(enclosing)),
+            ..Default::default()
+        }
+    }
+
     pub fn define(&mut self, name: &Token, value: Value) {
         self.values.insert(name.lexeme.clone(), value);
     }
 
     pub fn get(&self, name: &Token) -> Result<Value, Error> {
-        self.values
-            .get(&name.lexeme)
-            .cloned()
-            .ok_or(Error::runtime(name, format!("Undefined variable '{}'.", name.lexeme)))
+        if let Some(val) = self.values.get(&name.lexeme) {
+            Ok(val.clone())
+        } else if let Some(enc) = self.enclosing.as_ref() {
+            enc.get(&name)
+        } else {
+            Err(Error::runtime(
+                name,
+                format!("Undefined variable '{}'.", name.lexeme),
+            ))
+        }
     }
 
     pub fn assign(&mut self, name: &Token, value: Value) -> Result<Value, Error> {
@@ -30,8 +44,13 @@ impl Environment {
             *value_ref = value.clone();
 
             Ok(value)
+        } else if let Some(enc) = self.enclosing.as_mut() {
+            enc.assign(&name, value)
         } else {
-            Err(Error::runtime(name, format!("Undefined variable '{}'.", name.lexeme)))
+            Err(Error::runtime(
+                name,
+                format!("Undefined variable '{}'.", name.lexeme),
+            ))
         }
     }
 }
